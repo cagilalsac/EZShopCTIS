e970522f99ea583d9d102556668231ab068c846e
Çağıl Alsaç <cagilalsac@outlook.com>
2024-10-03 12:17:25 +0300

    Models (DTO) explained under BLL -> Models -> CategoryModel.

---
+++ BLL/Models/CategoryModel.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+
+using BLL.DAL;
+
+namespace BLL.Models
+{
+    public class CategoryModel
+    {
+        public Category Record { get; set; }
+
+        public string Name => Record.Name;
+
+        public string Description => Record.Description;
+
+        public string NameAndDescription => $"{Record.Name} {Record.Description}";
+    }
+}
