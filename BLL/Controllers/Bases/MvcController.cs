b16fa8c0766da9cd20f9e05573b27e15cae6aaf8
Çağıl Alsaç <cagilalsac@outlook.com>
2024-10-21 17:22:44 +0300

    MvcController base class created for Culture management. CategoriesController and views created by MVC scaffolding.

---
+++ BLL/Controllers/Bases/MvcController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace BLL.Controllers.Bases
+{
+    public abstract class MvcController : Controller
+    {
+        protected MvcController()
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        }
+    }
+}
--- MVC/Controllers/CategoriesController.cs
+++ MVC/Controllers/CategoriesController.cs
@@ -1,52 +1,132 @@
-﻿using BLL.Models;
-using BLL.Services;
+﻿#nullable disable
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BLL.Controllers.Bases;
+using BLL.Services;
+using BLL.Models;
+
+// Generated from Custom Template.
 
 namespace MVC.Controllers
 {
-    public class CategoriesController : Controller
+    public class CategoriesController : MvcController
     {
+        // Service injections:
         private readonly ICategoryService _categoryService;
 
-        public CategoriesController(ICategoryService categoryService)
+        /* Can be uncommented and used for many to many relationships. ManyToManyRecord may be replaced with the related entiy name in the controller and views. */
+        //private readonly IManyToManyRecordService _ManyToManyRecordService;
+
+        public CategoriesController(
+			ICategoryService categoryService
+
+            /* Can be uncommented and used for many to many relationships. ManyToManyRecord may be replaced with the related entiy name in the controller and views. */
+            //, IManyToManyRecordService ManyToManyRecordService
+        )
         {
             _categoryService = categoryService;
+
+            /* Can be uncommented and used for many to many relationships. ManyToManyRecord may be replaced with the related entiy name in the controller and views. */
+            //_ManyToManyRecordService = ManyToManyRecordService;
         }
 
+        // GET: Categories
         public IActionResult Index()
         {
+            // Get collection service logic:
             var list = _categoryService.Query().ToList();
             return View(list);
         }
 
+        // GET: Categories/Details/5
         public IActionResult Details(int id)
         {
-            //var category = _categoryService.Query().FirstOrDefault(c => c.Record.Id == id);
-            var category = _categoryService.Query().SingleOrDefault(c => c.Record.Id == id);
-            if (category is null) // ==: same as "is"
-                return NotFound();
-            return View(category);
+            // Get item service logic:
+            var item = _categoryService.Query().SingleOrDefault(q => q.Record.Id == id);
+            return View(item);
         }
 
-        //[HttpGet]
+        protected void SetViewData()
+        {
+            // Related items service logic to set ViewData (Record.Id and Name parameters may need to be changed in the SelectList constructor according to the model):
+            
+            /* Can be uncommented and used for many to many relationships. ManyToManyRecord may be replaced with the related entiy name in the controller and views. */
+            //ViewBag.ManyToManyRecordIds = new MultiSelectList(_ManyToManyRecordService.Query().ToList(), "Record.Id", "Name");
+        }
+
+        // GET: Categories/Create
         public IActionResult Create()
         {
+            SetViewData();
             return View();
         }
 
-        [HttpPost, ValidateAntiForgeryToken]
+        // POST: Categories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(CategoryModel category)
         {
             if (ModelState.IsValid)
             {
+                // Insert item service logic:
                 var result = _categoryService.Create(category.Record);
                 if (result.IsSuccessful)
                 {
-                    return RedirectToAction(nameof(Index));
+                    TempData["Message"] = result.Message;
+                    return RedirectToAction(nameof(Details), new { id = category.Record.Id });
                 }
                 ModelState.AddModelError("", result.Message);
             }
+            SetViewData();
             return View(category);
         }
-    }
+
+        // GET: Categories/Edit/5
+        public IActionResult Edit(int id)
+        {
+            // Get item to edit service logic:
+            var item = _categoryService.Query().SingleOrDefault(q => q.Record.Id == id);
+            SetViewData();
+            return View(item);
+        }
+
+        // POST: Categories/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryModel category)
+        {
+            if (ModelState.IsValid)
+            {
+                // Update item service logic:
+                var result = _categoryService.Update(category.Record);
+                if (result.IsSuccessful)
+                {
+                    TempData["Message"] = result.Message;
+                    return RedirectToAction(nameof(Details), new { id = category.Record.Id });
+                }
+                ModelState.AddModelError("", result.Message);
+            }
+            SetViewData();
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public IActionResult Delete(int id)
+        {
+            // Get item to delete service logic:
+            var item = _categoryService.Query().SingleOrDefault(q => q.Record.Id == id);
+            return View(item);
+        }
+
+        // POST: Categories/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            // Delete item service logic:
+            var result = _categoryService.Delete(id);
+            TempData["Message"] = result.Message;
+            return RedirectToAction(nameof(Index));
+        }
+	}
 }
